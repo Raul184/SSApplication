85f4500a0a4cc1e6cecd5ba186865a969315f70d
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-05-23 20:05:25 +0100

    tool to load init data into NoSQL_DB implemented & tested

--- models/tours.js
+++ models/tours.js
@@ -4,18 +4,52 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String ,
     required: [ true , 'Please provide a name' ] ,
-    unique: true 
+    unique: true ,
+    trim: true
   } ,
-  rating: {
+  durations: {
+    type: Number ,
+    required: [ true , 'How long does it take?' ]
+  },
+  maxGroupSize: {
+    type: Number ,
+    required: [ true , 'Please provide number of people' ]
+  },
+  difficulty: {
+    type: String ,
+    required: [ true , 'Please provide level of difficulty for our customers' ]
+  },
+  ratingsAverage: {
     type: Number ,
     default: 4.5 ,
-    max: 5 ,
-    min: 1
+  },
+  ratingsQuantity: {
+    type: Number ,
+    default: 0
   },
   price: {
     type: Number ,
     required: [ true , 'Please provide a price' ]
-  }
+  },
+  priceDiscount: Number ,
+  summary: {
+    type: String ,
+    trim: true
+  },
+  description: {
+    type: String ,
+    trim: true
+  },
+  imageCover: {
+    type: String ,
+    required: [ true , 'Please provide an image for the tour']
+  },
+  images: [ String ] ,
+  createdAt: {
+    type: Date ,
+    default: Date.now()
+  },
+  startDates: [ Date ],
 })
 
 const TourModel = mongoose.model('Tour' , tourSchema );
