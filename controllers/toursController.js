baaf3a61ccf6cc9396cdb9fa72438f59f67b0324
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-05-25 15:08:28 +0100

    Filter for tourPage complete

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -3,8 +3,51 @@ const TourModel = require('../models/tours');
 
 exports.getAllTours = async (req, res, next) => {
   try {
-    const tours = await TourModel.find()
+    // 1.a ) Filtering
+    const queryObj = { ...req.query }
+    const excludeFields = [ 'page' , 'sort' , 'limit' , 'fields' ]
+    excludeFields.forEach(el => delete queryObj[el])
+
+    // 1.b ) Advance Filtering
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g , match => `$${match}`);
     
+    // *
+    let query = TourModel.find(JSON.parse( queryStr ))
+    
+    // 2 ) Sorting
+    if(req.query.sort){
+      // + 1 filter ?
+      const allSorts = req.query.sort.split(',').join(' ')
+      query = query.sort( allSorts )
+    }
+    else{ // default sort implemented
+      query = query.sort( '-createdAt' )
+    }
+
+    // 3 ) Field Limiting
+    if(req.query.fields){
+      const fields = req.query.fields.split(',').join(' ')
+      query = query.select(fields)
+    } 
+    else {
+      query = query.select('-__v')
+    }
+
+    // 4 ) Pagination
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 1
+    const skip = ( page - 1 ) * limit
+
+    query = query.skip(skip).limit(limit)
+
+    if(req.query.page){
+      const numTours = await TourModel.countDocuments();
+      if( skip >= numTours ) throw new Error( 'Not more tours available')
+    }
+    // *
+    const tours = await query;
+
     return res.status(200).json({
       status: 'success' ,
       result: tours.length ,
@@ -16,7 +59,7 @@ exports.getAllTours = async (req, res, next) => {
   catch (error) {
     return res.status(404).json({
       status: 'failed' ,
-      msg: error
+      msg: error.message
     })  
   }
 }
