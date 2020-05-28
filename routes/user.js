6f357f3d7d2d9caf8be86138558d7c49e625bde6
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-05-28 23:10:19 +0100

    routes for Tour & Users controllers completed

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
 const usersController = require('./../controllers/usersController');
+const authController = require('./../controllers/authController');
+
 const {
   getAllUsers ,
   add1User ,
@@ -8,8 +11,12 @@ const {
   delete1User ,
   get1User
 } = usersController
+const { signup } = authController;
 
 
+router.post( '/signup' , signup )
+
+//  ADMIN 
 router.get('/', getAllUsers );
 router.post('/', add1User );
 router.get( '/:id' , get1User );
