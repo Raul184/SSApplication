491dd32f1264e4dad6875d8933b03b98702eb850
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-06-17 16:48:03 +0100

    mapBox showing locations from tour selected on tourDetailsPg

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,44 @@
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations)
 console.log(locations);
+
+mapboxgl.accessToken = 
+'pk.eyJ1IjoibXVyY2llZ2FsbyIsImEiOiJja2JqaWFtemEwbmF1MnJ0ZHJsNXFiOTByIn0.HCRDiCTZiILWMA7ZmzfYDQ';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  scrollZoom:false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(el => {
+  //1. add markers
+  const marker = document.createElement('div');
+  marker.className = 'marker';
+  new mapboxgl.Marker({
+    element: marker,
+    anchor: 'bottom'
+  })
+  //2. set markers
+  .setLngLat(el.coordinates)
+  .addTo(map);
+
+  // PopUp
+  new mapboxgl.Popup({offset:30})
+  .setLngLat(el.coordinates)
+  .setHTML(`<p>Day ${el.day}:${el.description}</p>`)
+  .addTo(map);
+  //Include current location on map
+  bounds.extend(el.coordinates)
+});
+
+map.fitBounds(bounds, {
+  padding:{
+    top: 200,
+    bottom: 200,
+    left: 200,
+    right: 100
+  }
+});
