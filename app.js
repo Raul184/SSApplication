8f5dd764e80c1151b78e329ecf694e26ea3ac8b4
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-06-19 15:50:07 +0100

    Auth now protects routes on req & cookies

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser')
 //Errors
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,7 +39,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
-
+app.use(cookieParser());
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
@@ -63,7 +64,8 @@ app.use(express.static(path.join(__dirname , 'public')));
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  // checking for JWT being sent (dev)
+  console.log(req.cookies);
   next();
 });
 
