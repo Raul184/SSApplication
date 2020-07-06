240337739f6ef002e35f951d029a084e9cd63c29
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-06 20:08:06 +0100

    images now resized while on req. cicle

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,21 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: (req,file,cb) => {
-    cb(null, 'public/img/users')
-  },
-  filename: (req,file,cb) => {
-    //user-11adfe1E32HJU21321a-332333545.jpeg
-    const ext = file.mimetype.split('/')[1]
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
-  }
-})
+// const multerStorage = multer.diskStorage({
+//   destination: (req,file,cb) => {
+//     cb(null, 'public/img/users')
+//   },
+//   filename: (req,file,cb) => {
+//     //user-11adfe1E32HJU21321a-332333545.jpeg
+//     const ext = file.mimetype.split('/')[1]
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
+//   }
+// })
+const multerStorage = multer.memoryStorage()
 const multerFilter = (req,file, cb) => {
   if(file.mimetype.startsWith('image')){
     cb(null, true)
@@ -29,7 +31,6 @@ const upload= multer({
   fileFilter: multerFilter
 })
 
-exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -39,6 +40,19 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.uploadUserPhoto = upload.single('photo');
+exports.resizeUserPhoto = (req,res,next) => {
+  if(!req.file) return next()
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
+  //memory & read
+  sharp(req.file.buffer)
+    .resize(500,500)
+    .toFormat('jpeg')
+    .jpeg({quality:90})
+    .toFile(`public/img/users/${req.file.filename}`)
+
+  next() 
+}
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
