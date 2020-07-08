cbc5b7e3c63cf8123489270299b68bc76bde603a
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-08 10:35:43 +0100

    complex email handler in place

--- public/bundle.js
+++ public/bundle.js
@@ -9001,7 +9001,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52359" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57610" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
