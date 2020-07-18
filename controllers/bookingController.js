ac5ac1c190c4831d02be3536c1186b81d19ce092
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-18 15:10:46 +0100

    last touches on stripeCheckoutSession settings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,11 +1,12 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
-// const Booking = require('../models/bookingModel');
+const Booking = require('../models/bookingsModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('./../utils/appError')
 
+// STRIPE 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourID);
@@ -19,17 +20,17 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
-        ],
+        // images: [
+        //   `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        // ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
       }
     ],
     mode:'payment',
-    success_url: `${req.protocol}://${req.get('host')}/`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`
+    success_url: `${req.protocol}://${req.get('host')}/me`,
+    cancel_url: `${req.protocol}://${req.get('host')}/`
   });
 
   // 3) Create session as response
