663a6e876e9a51be312397b539b17bc52444bb11
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-18 14:04:35 +0100

    booking model set

---
+++ models/bookingsModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true , 'A booking needs a selected tour']
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true , 'A booking needs a selected tour']
+  },
+  price: {
+    type: Number,
+    required: [true , 'Please check the price for your booking']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  paid: {
+    type: Boolean,
+    default: true
+  }
+});
+// Query Middleware
+bookingSchema.pre(/^find/, function(next){
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name'
+  })
+})
+
+const BookingModel = mongoose.model('Booking', bookingSchema);
+
+module.exports = BookingModel;
