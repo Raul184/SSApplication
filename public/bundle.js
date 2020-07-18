14a92fcfd26de4657e28c55871a328ba75d656d4
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-18 15:02:32 +0100

    small bug fixed on stripeCheckout

--- public/bundle.js
+++ public/bundle.js
@@ -8791,72 +8791,51 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var setStripe = /*#__PURE__*/function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    var strip;
+var stripePromise = (0, _stripeJs.loadStripe)('pk_test_qpk8XXIK4nkQNnyLqJ3RH5Mt009GgCH5Lr');
+
+var bookTour = /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(tourId) {
+    var session, stripe;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.next = 2;
-            return (0, _stripeJs.loadStripe)('pk_test_qpk8XXIK4nkQNnyLqJ3RH5Mt009GgCH5Lr');
-
-          case 2:
-            return _context.abrupt("return", strip = _context.sent);
-
-          case 3:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  }));
-
-  return function setStripe() {
-    return _ref.apply(this, arguments);
-  };
-}();
-
-var stripe = setStripe();
-
-var bookTour = /*#__PURE__*/function () {
-  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(tourId) {
-    var session;
-    return regeneratorRuntime.wrap(function _callee2$(_context2) {
-      while (1) {
-        switch (_context2.prev = _context2.next) {
-          case 0:
-            _context2.prev = 0;
-            _context2.next = 3;
+            _context.prev = 0;
+            _context.next = 3;
             return (0, _axios.default)("/api/v1/bookings/checkout-session/".concat(tourId));
 
           case 3:
-            session = _context2.sent;
-            _context2.next = 6;
+            session = _context.sent;
+            _context.next = 6;
+            return stripePromise;
+
+          case 6:
+            stripe = _context.sent;
+            _context.next = 9;
             return stripe.redirectToCheckout({
               sessionId: session.data.session.id
             });
 
-          case 6:
-            _context2.next = 12;
+          case 9:
+            _context.next = 15;
             break;
 
-          case 8:
-            _context2.prev = 8;
-            _context2.t0 = _context2["catch"](0);
-            console.log(_context2.t0);
-            (0, _alert.showAlert)('error', _context2.t0);
+          case 11:
+            _context.prev = 11;
+            _context.t0 = _context["catch"](0);
+            console.log(_context.t0);
+            (0, _alert.showAlert)('error', _context.t0);
 
-          case 12:
+          case 15:
           case "end":
-            return _context2.stop();
+            return _context.stop();
         }
       }
-    }, _callee2, null, [[0, 8]]);
+    }, _callee, null, [[0, 11]]);
   }));
 
   return function bookTour(_x) {
-    return _ref2.apply(this, arguments);
+    return _ref.apply(this, arguments);
   };
 }();
 
@@ -9258,7 +9237,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60891" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50397" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 import {loadStripe} from '@stripe/stripe-js';
 import { showAlert } from './alert';
-const setStripe = async () => {
-  let strip;
-  return strip = await loadStripe('pk_test_qpk8XXIK4nkQNnyLqJ3RH5Mt009GgCH5Lr'); 
-}
-const stripe = setStripe()
+const stripePromise = loadStripe('pk_test_qpk8XXIK4nkQNnyLqJ3RH5Mt009GgCH5Lr');
+
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    
     // 2) Create checkout form + charge credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
-    });
+    const stripe = await stripePromise;
+      await stripe.redirectToCheckout({
+        sessionId: session.data.session.id
+      });
   } 
   catch (err) {
     console.log(err);
